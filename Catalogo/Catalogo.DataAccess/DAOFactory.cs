91bc66ad71930147affe3e7a37dab5924e3f7ed5
Anderson Dourado <anderson.ictus@gmail.com>
2018-03-03 11:02:59 -0300

    0.0.1-alpha - DataAccess e Geral ---------------------------------- - Adicionando um pequeno arquivo de Banco de Dados em SQL (Para simular uma conexao com uma Tabela Real) - Incluido aruqivos de Configuracao Para o Acesso - Incluindo Os arquivos de acesso ao Banco de Dados - Criando dois Modelos Basicos Produto e Catalogo    - Criando arquivo CategoriaDAO - Criando a interface IDataAccess para paronizar os objetos de acesso aos dados (ele utiliza um Generico para indicar o Tipo de DataAccess em tempo de execucao) - Criando a Classe DAOFactory para encapsular a criação de um DAO para os dados !

---
+++ Catalogo/Catalogo.DataAccess/DAOFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Catalogo.DataAccess
+{
+    public class DAOFactory
+    {
+       private DAOFactory()
+        { }
+
+        public static IDataAccess<Categoria> getCategoriaDAO()
+        {
+            return new DAO.CategoriaDAO();
+        }
+
+        public static IDataAccess<Produto> getProdutoDAO()
+        {
+            return null; // new DAO.CategoriaDAO();
+        }
+    }
+}
---
+++ Catalogo/Catalogo.DataAccess/IDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogo.DataAccess
+{
+    public interface IDataAccess<M>
+    {
+        bool incluir(M model);
+        bool deletar(M model);
+        bool deletarById(int idModel);
+        bool atualizar(M model);
+        M retornar(int idModel);
+        List<M> retornarAll();
+    }
+}
