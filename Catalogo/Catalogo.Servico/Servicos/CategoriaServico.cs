886c9a6edc1a8bac2ecde618e96e03a0fa4f38b6
Anderson Dourado <anderson.ictus@gmail.com>
2018-03-03 11:00:56 -0300

    0.0.0-alpha - Global ----------------------------------
    - Commit INicial para os projetos que serao desenvolvidos
      - A escolha inicial foi para construir a aplicacao em 3 CAMADAS
      CAMADA 1: DataAccess
        * Essa camada sera a responsavel por acessar o banco de dados e prover os MODELOS de dados necessario pela aplicação
    
      Camada 2: Servico
        * Essa camada eh a camada de Controller que provê uma FACHADA de Servicos Web dos Modelos da Camada 1
        * Essa Camada tem Visao dos MODELOS da CAMADA 1
    
      CAMADA 3: Aplicacao
        * Essa camada eh responsavel por acessar o servico da CAMADA 2 e ser vir como visão das respostas do Servico Web
        * Essa camada tem Visao dos MODELOS da CAMADA 1, mas não se comunica diretamente com essa camada
        * A ideia aqui é ter Controladores que fazem acesso ao Servico Web, e a partir desses servicos a Aplicação fazer uso dos dados
    
    - Os arquivos adicionados aqui são necessários para um commit inicial do projeto e os proximos commits terao prioridade por camadas, no qual
       * Insercoes na camada 1 tera sufixo DataAccess
       * Insercoes na camada 2 tera sufixo Servico
       * Insercoes na camada 3 tera sufixo Aplicacao
       * Insercoes gerais (mudanças entre camdas) tera sufixo Geral
    
    - Apesar de termos 3 projetos, somente uma versao sera adotada, seguindo a seguinte logica:
    MINOR VERSION: Para ajustes pequenos, ou refatoracao em alguma camada
    MIDLLE VERSION: Para fechamento de algum processo ou implementação importante
    MAJOR VERSION: Para fechamento de versão ou alguma inclusão muito grande

---
+++ Catalogo/Catalogo.Servico/Servicos/CategoriaServico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Catalogo.Servico.Servicos
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "CategoriaServico" no arquivo de código e configuração ao mesmo tempo.
+    public class CategoriaServico : ICategoriaServico
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ Catalogo/Catalogo.Servico/Servicos/ProdutoServico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Catalogo.Servico.Servicos
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "ProdutoServico" no arquivo de código e configuração ao mesmo tempo.
+    public class ProdutoServico : IProdutoServico
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
