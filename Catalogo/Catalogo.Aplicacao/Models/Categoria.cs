f5189119395f986dd7a3fed8107b71504d5ab2e6
Anderson Dourado <anderson.ictus@gmail.com>
2018-03-04 18:31:24 -0300

    * 0.0.4 -alpha - Aplicacao ---------------------------------- - Inclusao dos arquivos para a aplicacao ASP.NET MVC
    - Controladores:
        - CatalogoController -> Para a tela inicial da aplicacao
        - CategoriaController -> Para a Manipulacao de categoria
        - ProdutoController -> Para a Manipulacao de Produtos
    
    - View:
       - Catalogo -> Pasta com as views referentes as acoes do CatalogoController
       - Categoria -> Pasta com as views referentes as acoes do CategoriaController
       - Produto -> Pasta com as views referentes as acoes do ProdutoController
    
    - Models:
       - Categoria -> Um modelo inicial que sera substituido quando houver o bind final com o servico.
    
    - Tambem esta sendo incluido arquivos padroes para a aplicacao e que serao necessarios para o desenvolvimento das paginas

---
+++ Catalogo/Catalogo.Aplicacao/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Catalogo.App.Models
+{
+    public class Categoria
+    {        
+        public int idCat { get; set; }
+
+        [Required]
+        public string nomeCat { get; set; }
+    }
+}
