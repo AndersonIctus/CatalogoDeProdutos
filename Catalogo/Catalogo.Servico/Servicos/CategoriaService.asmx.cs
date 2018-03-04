03700227fb4c6bb8015efd64fb452a19b7296465
Anderson Dourado <anderson.ictus@gmail.com>
2018-03-03 21:04:49 -0300

    * 0.0.3 -alpha - Servico ---------------------------------- - Inclusao dos arquivos para fazer a fachada Webservice (essa fachada funciona como CAMADA de Negocios)
    - Aproveitando para fazer algumas mudanças necessarias dentro do projeto
      - Removido as classes de pagina inicial que sao desnecessarios
      - Removendo a Solucao com os projeto e criando outras duas
          * CatalogoServico.slt -> Ficara a classe de servicoWeb e acesso a esquemaSQL de banco de Dados
          * CatalogoAplicacao -> Onde ficara o projeto de aplicacao

---
+++ Catalogo/Catalogo.Servico/Servicos/CategoriaService.asmx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Catalogo.DataAccess;
+
+namespace Catalogo.Servico.Servicos
+{
+    /// <summary>
+    /// Descrição resumida de CategoriaService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CategoriaService : System.Web.Services.WebService
+    {
+        IDataAccess<Categoria> dao = DAOFactory.getCategoriaDAO();
+
+        [WebMethod]
+        public Categoria retornarById(int idCat)
+        {
+            Categoria cat = dao.retornar(idCat);
+            if(cat == null)
+            {
+                cat = new Categoria { idCat = -1, nomeCat = "Id inexistente ou Falha na Conexao" };
+            }
+
+            return cat;
+        }
+
+        [WebMethod]
+        public List<Categoria> retornarAll()
+        {
+            return dao.retornarAll();
+        }
+
+        [WebMethod]
+        public bool incluir(Categoria cat)
+        {
+            return dao.incluir(cat);
+        }
+
+        [WebMethod]
+        public bool atualizar(Categoria cat)
+        {
+            return dao.atualizar(cat);
+        }
+
+        [WebMethod]
+        public bool deletar(Categoria cat)
+        {
+            return dao.deletar(cat);
+        }
+
+        [WebMethod]
+        public bool deletarById(int idCat)
+        {
+            return dao.deletarById(idCat);
+        }
+    }
+}
